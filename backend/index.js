a58f75291ebb931ab4c91292a50765ba202a47de
t3rran0va <158166791+t3rran0va@users.noreply.github.com>
2025-02-28 15:46:02 +0000

    Resolved conflict in backend/streak.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
+// code for terminal
+//npm init -y
+//npm install express pg pg-hstore sequelize dotenv
 
+
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+const { Sequelize } = require("sequelize");
--- backend/streak.js
+++ backend/streak.js
@@ -1,1 +1,20 @@
 
+const {Streak}  = require("../database");
+
+
+async function getStreak(){
+    //let streak = await Streak.findByPK(1);
+    //find the streak by userID?
+    if (!streak){
+        streak = await Streak.create();
+    }
+    return streak;
+}
+
+async function updateStreak(){
+    let streak = await getStreak();
+    const today = new Date();
+    //let lastUpdate when the user last loged on;
+    //if user logged in today
+    //{streak.streak_count += 1}
+}
