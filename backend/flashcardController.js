ccf035f9b862fd90c34b02a91dc585a18e7bc06e
Habeeb Bolaji <up2132180@myport.ac.uk>
2025-05-06 00:00:18 +0100

    leaderboard/flashcards backend functioning

--- backend/flashcardController.js
+++ backend/flashcardController.js
@@ -9,9 +9,8 @@ const { pool } = require("./database-pool"); // Adjust path as necessary
 async function createFlashcard({ userId, term, definition, colour }) {
     try {
         const result = await pool.query(
-
-            `INSERT INTO flashcards (user_id, flashcard_term, flashcard_definition, flashcard_colour)
-            VALUES ($1, $2, $3, $4)
+            `INSERT INTO flashcards (user_id, term, definition, colour)
+             VALUES ($1, $2, $3, $4)
              RETURNING *`,
             [userId, term, definition, colour]
         );
